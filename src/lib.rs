ddca82e5fc48764223b59d2180067a1dcf03fa5d
Error-42 <erroruplus002d42@gmail.com>
2024-06-12 13:27:17 +0200

    removed unused use

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(mixed_script_confusables)]
 //! Only the binary may be stable, the library cannot!
-use std::{collections::HashMap, error::Error, fs, path::Path, thread::scope};
+use std::{collections::HashMap, error::Error, fs, path::Path};
 
 use nalgebra::{DMatrix, DVector};
 use serde::{Deserialize, Serialize};
