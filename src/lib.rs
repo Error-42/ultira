b2750510f564b233c91119a83af11f886f6ea938
Error-42 <erroruplus002d42@gmail.com>
2024-09-10 17:04:05 +0200

    fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -432,7 +432,7 @@ impl Renamable for Circular {
 }
 
 #[derive(Debug, Clone, Default, PartialEq, Eq, Deserialize, Serialize)]
-/// Must contain at leats 1 player, but probably should contain at least 3. TODO: check this?
+/// Must contain at least 1 player, but probably should contain at least 3. TODO: check this?
 pub struct Symmetric {
     pub date: chrono::NaiveDate,
     pub scores: HashMap<String, i64>,
